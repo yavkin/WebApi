75472ee68f49178ae190a3f4f9d93f03af3f335f
yavkin <yavckin.vladislav@gmail.com>
2022-12-22 02:52:04 +0300

    2 entities

--- laba1/Repository/RepositoryManager.cs
+++ laba1/Repository/RepositoryManager.cs
@@ -11,6 +11,8 @@ namespace Repository
         private RepositoryContext _repositoryContext;
         private ICompanyRepository _companyRepository;
         private IEmployeeRepository _employeeRepository;
+        private IRealtyCompanyRepository _realtycompanyRepository;
+        private IClientRepository _clientRepository;
         public RepositoryManager(RepositoryContext repositoryContext)
         {
             _repositoryContext = repositoryContext;
@@ -31,6 +33,24 @@ namespace Repository
             if (_employeeRepository == null)
                     _employeeRepository = new EmployeeRepository(_repositoryContext);
                 return _employeeRepository;
+            }
+        }
+        public IRealtyCompanyRepository RealtyCompany
+        {
+            get
+            {
+                if (_realtycompanyRepository == null)
+                    _realtycompanyRepository = new RealtyCompanyRepository(_repositoryContext);
+                return _realtycompanyRepository;
+            }
+        }
+        public IClientRepository Client
+        {
+            get
+            {
+                if (_clientRepository == null)
+                    _clientRepository = new ClientRepository(_repositoryContext);
+                return _clientRepository;
             }
         }
         public void Save() => _repositoryContext.SaveChanges();
